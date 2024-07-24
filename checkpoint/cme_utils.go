bbe02f17a7d223dc479f7558af0ffde828bf394a
nimrodgab <nimrodgab@checkpoint.com>
2024-07-24 14:22:05 +0300

    Draft: Add validation for cme api version for cme resources

--- checkpoint/cme_utils.go
+++ checkpoint/cme_utils.go
@@ -1,14 +1,44 @@
 package checkpoint
 
 import (
+	"fmt"
+	checkpoint "github.com/CheckPointSW/cp-mgmt-api-go-sdk/APIFiles"
 	"math"
 	"strconv"
 )
 
 const (
-	CmeApiVersion = "v1.1"
-	CmeApiPath    = "cme-api/" + CmeApiVersion
+	ProviderCmeApiVersion = "v1.2"
+	CmeApiPath            = "cme-api/" + ProviderCmeApiVersion
 )
+
+
+func validateCmeApiVersion(c *checkpoint.ApiClient) error {
+	apiVersion, err := getCmeApiVersion(c)
+	if err != nil {
+		return fmt.Errorf(err.Error())
+	}
+	if ProviderCmeApiVersion > apiVersion {
+		return fmt.Errorf("invalid Provider CME API version, it must be equal or lower than CME API version in" +
+			" the Management. Provider CME API version: %s, CME API version in the Management: %s", ProviderCmeApiVersion, apiVersion)
+	}
+	return nil
+}
+
+func getCmeApiVersion(c *checkpoint.ApiClient) (string, error) {
+	url := "cme-api/api-versions"
+	res, err := c.ApiCall(url, nil, c.GetSessionID(), true, false, "GET")
+	if err != nil {
+		return "", fmt.Errorf(err.Error())
+	}
+	cmeAPIVersionsJson := res.GetData()
+	if checkIfRequestFailed(cmeAPIVersionsJson) {
+		errMessage := buildErrorMessage(cmeAPIVersionsJson)
+		return "", fmt.Errorf(errMessage)
+	}
+	ApiVersions := cmeAPIVersionsJson["result"].(map[string]interface{})
+	return ApiVersions["current_version"].(string), nil
+}
 
 func checkIfRequestFailed(resJson map[string]interface{}) bool {
 
